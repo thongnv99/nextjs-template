09fc55e913e1597ed7f896f01ddb00dbdc6ac61a
thongnv <thong.nv173390@gmail.com>
2024-01-11 01:48:14 +0700

    add logic query question

--- src/components/QuestionItem/index.tsx
+++ src/components/QuestionItem/index.tsx
@@ -4,8 +4,18 @@ import Trash from 'assets/svg/trash.svg';
 import Edit from 'assets/svg/edit.svg';
 import Copy from 'assets/svg/copy.svg';
 import Chevron from 'assets/svg/chevron-down.svg';
+import { IQuestion } from 'interfaces';
+import { QUESTION_TYPE } from 'global';
+import { formatNumber } from 'utils/common';
+import { formatDateToString } from 'utils/datetime';
 
-type Props = {};
+type Props = { data: IQuestion };
+
+const mapQuestionType = {
+  [QUESTION_TYPE.ESSAY]: 'Tự luận',
+  [QUESTION_TYPE.MULTIPLE_CHOICE]: 'Trắc nghiệm',
+  [QUESTION_TYPE.FILL_IN_THE_BLANK]: 'Điền vào chỗ trống',
+};
 
 const QuestionItem = (props: Props) => {
   const handleEdit = (event: MouseEvent) => {
@@ -29,9 +39,10 @@ const QuestionItem = (props: Props) => {
             <Disclosure.Button>
               <div className="flex items-center justify-between p-4 rounded-lg border transition duration-75 border-gray-200 shadow-sm">
                 <div className="flex flex-col justify-between items-start">
-                  <div className="text-base text-left text-gray-900 font-semibold">
-                    Tiêu đề câu hỏi
-                  </div>
+                  <div
+                    className="text-base text-left text-gray-900 font-semibold"
+                    dangerouslySetInnerHTML={{ __html: props.data.content }}
+                  ></div>
                   <div className="text-sm text-gray-500 font-normal">
                     Up to 10 users and 20GB individual data.
                   </div>
@@ -65,19 +76,24 @@ const QuestionItem = (props: Props) => {
                     <div className="flex">
                       <div className="min-w-[10rem] font-semibold">Loại</div>
                       <div className="font-normal text-sm text-gray-500">
-                        Trắc nghiệm
+                        {mapQuestionType[props.data.type]}
                       </div>
                     </div>
                     <div className="flex">
                       <div className="min-w-[10rem] font-semibold">Điểm</div>
-                      <div className="font-normal text-sm text-gray-500">1</div>
+                      <div className="font-normal text-sm text-gray-500">
+                        {formatNumber(props.data.score ?? 1)}
+                      </div>
                     </div>
                     <div className="flex">
                       <div className="min-w-[10rem] font-semibold">
                         Ngày tạo
                       </div>
                       <div className="font-normal text-sm text-gray-500">
-                        13/11/2023 22:13:21
+                        {formatDateToString(
+                          new Date(props.data.createdAt),
+                          'dd/MM/yyyy HH:mm:ss',
+                        )}
                       </div>
                     </div>
                   </div>
